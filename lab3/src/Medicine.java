48aa2bb01fd63251b85670ce0d359c67b893daa3
Alikhawari <56734151+Alikh-2617@users.noreply.github.com>
2020-11-09 19:05:02 +0100

    cod

---
+++ lab3/src/Medicine.java
@@ -0,0 +1,11 @@
+public class Medicine {
+    public String treatsSickness;
+
+    public Medicine(String sickness) {
+        this.treatsSickness = sickness;
+    }
+
+    public String getTreatmentName() {
+        return treatsSickness;
+    }
+}
